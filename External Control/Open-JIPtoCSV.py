b550d844bbc61b6eccfee33a13850802f948bf34
Harvey Bates <phenobottle@gmail.com>
2020-12-18 11:06:40 +1100

    fixed indentation error

--- External Control/Open-JIPtoCSV.py
+++ External Control/Open-JIPtoCSV.py
@@ -1,4 +1,4 @@
-# 
+#
 import sys
 import csv
 import glob
@@ -7,13 +7,13 @@ import time
 import datetime
 from time import strftime
 
-usb_baudrate = 115200 # Baudrate to match Teensy
-fileName = "Open-JIP_Data.csv" # Filename of output .csv file
+usb_baudrate = 115200  # Baudrate to match Teensy
+fileName = "Open-JIP_Data.csv"  # Filename of output .csv file
 
 
 def serial_ports():
-	# List serial ports available on each OS
-	# For windows OS
+        # List serial ports available on each OS
+        # For windows OS
     if sys.platform.startswith('win'):
         ports = ['COM%s' % (i + 1) for i in range(256)]
     # For linux OS
@@ -29,72 +29,80 @@ def serial_ports():
         try:
             s = serial.Serial(port)
             s.close()
-            if("Bluetooth" not in port): # Excludes bluetooth port on Mac OS
-            	result.append(port)
+            if("Bluetooth" not in port):  # Excludes bluetooth port on Mac OS
+                result.append(port)
         except (OSError, serial.SerialException):
             pass
     if(len(result) == 0):
-    	raise IndexError("No USB devices found. Ensure Open-JIP is pugged in.")
+        raise IndexError("No USB devices found. Ensure Open-JIP is pugged in.")
     else:
-    	return result
+        return result
 
 
 def connect(portAddress):
-	# Connect to the Open-JIP fluorometer (Teensy Microcontroller)
-	global ser
-	if(len(portAddress) > 1 and type(portAddress) is not str):
-		print("Which port do you want to connect to:")
-		for i in range(len(portAddress)):
-			print("\t{}. {}".format(i, portAddress[i]))
-		portID = input("Port number: ")
-		portID = int(portID)
-		print("Port {} selected.".format(portAddress[portID]))
-	else:
-		portID = 0
-	ser = serial.Serial(portAddress[portID], usb_baudrate) # Connect to Teensy
-	if(ser.is_open):
-		print("Connected to Open-JIP fluorometer.")
-	else:
-		print("Open-JIP USB device not found.")
+        # Connect to the Open-JIP fluorometer (Teensy Microcontroller)
+    global ser
+    if(len(portAddress) > 1 and type(portAddress) is not str):
+        print("Which port do you want to connect to:")
+        for i in range(len(portAddress)):
+            print("\t{}. {}".format(i, portAddress[i]))
+        portID = input("Port number: ")
+        portID = int(portID)
+        print("Port {} selected.".format(portAddress[portID]))
+    else:
+        portID = 0
+    ser = serial.Serial(portAddress[portID], usb_baudrate)  # Connect to Teensy
+    if(ser.is_open):
+        print("Connected to Open-JIP fluorometer.")
+    else:
+        print("Open-JIP USB device not found.")
 
 
 def measure_fluorescence(readLength):
-	# Read fluorescence and create two arrays of corresponding values
-	print("Measuring fluorescence, please wait...")
-	ser.flush() # Clear serial bus
-	time.sleep(1) # Wait for bus to be cleared
-	ser.write(b'MF') # Send byte command to Teensy 
-	timeStamps = [] # Create array to hold timeStamp
-	fluorescenceValues = [] # Create array to hold data
-	for _ in range(readLength):
-		line = ser.readline() # Read line from Teensy serial bus
-		decodedLine = str(line[0:len(line) - 2].decode("utf-8")) # Decde data from serial bus
-		splitLine = [float(s) for s in decodedLine.split("\t")] # Split into time and values
-		timeStamps.append(splitLine[0])
-		fluorescenceValues.append(float(splitLine[1]))
-	print("Transient captured.")
-	return timeStamps, fluorescenceValues # Return arrays to be passed into csv upload function
+    # Read fluorescence and create two arrays of corresponding values
+    print("Measuring fluorescence, please wait...")
+    ser.flush()  # Clear serial bus
+    time.sleep(1)  # Wait for bus to be cleared
+    ser.write(b'MF')  # Send byte command to Teensy
+    timeStamps = []  # Create array to hold timeStamp
+    fluorescenceValues = []  # Create array to hold data
+    for _ in range(readLength):
+        line = ser.readline()  # Read line from Teensy serial bus
+        # Decde data from serial bus
+        decodedLine = str(line[0:len(line) - 2].decode("utf-8"))
+        # Split into time and values
+        splitLine = [float(s) for s in decodedLine.split("\t")]
+        timeStamps.append(splitLine[0])
+        fluorescenceValues.append(float(splitLine[1]))
+    print("Transient captured.")
+    # Return arrays to be passed into csv upload function
+    return timeStamps, fluorescenceValues
 
 
 def upload(timeStamps, fluorescenceValues):
-	# Upload data from fluorescence reading to .csv file with time stamp
-	print("Uploading to {}, please wait...".format(fileName))
-	timeStamps = ", ".join(str(x) for x in timeStamps) # Join array into a single string seperated by a comma
-	fluorescenceValues = ", ".join(str(x) for x in fluorescenceValues) # Same as above for the values
-	with open(fileName, 'a') as f:
-		try:
-			writer = csv.writer(f)
-			currentTime = str(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")) # Get current time for csv
-			writer.writerow([currentTime, timeStamps, fluorescenceValues]) # Upload data to a single row
-			print("Data written to {}.".format(fileName))
+    # Upload data from fluorescence reading to .csv file with time stamp
+    print("Uploading to {}, please wait...".format(fileName))
+    # Join array into a single string seperated by a comma
+    timeStamps = ", ".join(str(x) for x in timeStamps)
+    # Same as above for the values
+    fluorescenceValues = ", ".join(str(x) for x in fluorescenceValues)
+    with open(fileName, 'a') as f:
+        try:
+            writer = csv.writer(f)
+            currentTime = str(datetime.datetime.now().strftime(
+                "%Y/%m/%d %H:%M:%S"))  # Get current time for csv
+            # Upload data to a single row
+            writer.writerow([currentTime, timeStamps, fluorescenceValues])
+            print("Data written to {}.".format(fileName))
 
-		except:
-			print("Failed to write data to {}.".format(fileName))
-	f.close()
+        except:
+            print("Failed to write data to {}.".format(fileName))
+    f.close()
+
 
 if __name__ == "__main__":
-	port = serial_ports()
-	connect(port)
-	timeStamps, fluorescenceValues = measure_fluorescence(2000) # Takes the length of the corresponding array in the Teensy script
-	upload(timeStamps, fluorescenceValues)
-
+    port = serial_ports()
+    connect(port)
+    # Takes the length of the corresponding array in the Teensy script
+    timeStamps, fluorescenceValues = measure_fluorescence(2000)
+    upload(timeStamps, fluorescenceValues)
